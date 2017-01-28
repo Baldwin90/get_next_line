4257053dc0548ea0f692ad704cfc57dd49f9e93f
Patrick <Lav0c@yahoo.com>
2017-01-27 22:55:58 -0800

    created get new_file function

--- get_next_line.c
+++ get_next_line.c
@@ -4,7 +4,39 @@
 #include <unistd.h>
 #include "get_next_line.h"
 
-int		get_next_line(const int fd, char **line)
+int				get_next_line(const int fd, char **line)
 {
-	asdf
+	t_file *file;
+
+	if (!(file = new_file(fd)))
+	{
+		return (-1); // I think this means it wasnt able to read from the file
+	}
+	file 
+}
+
+t_file *new_file(const int fd)
+{
+	V;
+	if (fd < 0)
+		return (NULL);
+	while (i <= total)
+	{
+		if (i == total && total < F_COUNT_MAX)
+		{
+			total += 1;
+			file[i].fd = fd;
+			file[i].index = 0;
+			while (j < BUFF_SIZE)
+			{
+				file[i].buff[j] = 0;
+				j += 1;
+			}
+			return (&file[i]);
+		}
+		else if (file[i].fd == fd)
+			return (&file[i]);
+		i += 1;
+	}
+	return (NULL);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -1,4 +1,25 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-// your stuff
+
+#include "libft/includes/libft.h"
+
+# define TRUE			1
+# define FALSE			0
+# define BUFF_SIZE		2048
+# define F_COUNT_MAX	64
+# define I	static
+# define V	I t_file file[F_COUNT_MAX]; I int total = 0; int i = 0; int j = 0;
+/*
+** might not be needed
+** # define BUFF_SIZE_TOTAL	(BUFF_SIZE * F_COUNT_MAX)
+*/
+
+typedef		s_file
+{
+	int		fd;
+	int		index;
+	char	buff[BUFF_SIZE];
+}			t_file;
+
+int		get_next_line(const int fd, char **line);
 #endif
