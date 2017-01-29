77f5f96a7699dbd6dbe07b9dd8c99c2b925e4f23
Patrick <Lav0c@yahoo.com>
2017-01-28 19:04:33 -0800

    this one will norm

--- get_next_line.c
+++ get_next_line.c
@@ -4,7 +4,10 @@
 #include <unistd.h>
 #include "get_next_line.h"
 
-#include <stdio.h> // DELETE ME!!!
+# define FU int i = -1;
+# define BS else if(BUF[i+BUF_IND]=='\n'){grow_string(file,&i,&result,
+# define GARBAGE &result_len);break;}if(N_BYTES_READ == 0){return(result);}
+# define NORM BS GARBAGE;
 
 t_file 	*new_file(const int fd)
 {
@@ -54,12 +57,9 @@ void	grow_string(t_file *file, int *i, char **result, int *result_len)
 
 char	*store_line(t_file *file, char *result, int result_len, int *r_code)
 {
-	int i;
-
-	i = -1;
-	// if nothing is in the buffer and you get nothing from reading from the file then the file is done
-	// so you should return an empty string and a return code of 0
-	if (BUF_IND >= N_BYTES_READ && (N_BYTES_READ = read(file->fd, BUF, BUFF_SIZE - 1)) == 0)
+	FU;
+	if (BUF_IND >= N_BYTES_READ && (N_BYTES_READ =
+		read(file->fd, BUF, BUFF_SIZE - 1)) == 0)
 	{
 		*r_code = 0;
 		return (result);
@@ -67,39 +67,18 @@ char	*store_line(t_file *file, char *result, int result_len, int *r_code)
 	while (TRUE)
 	{
 		*r_code = 1;
-		// if you've indexed to the buffer size or the number of stored bytes you need to
-		// copy the buffer, statring at the buf_ind to the end of the current result
 		if (++i + BUF_IND >= BUFF_SIZE || i + BUF_IND >= N_BYTES_READ)
 		{
 			grow_string(file, &i, &result, &result_len);
 			BUF_IND = 0;
-			// you need to refresh the buffer with new characters
-			// if the you get back nothing then you reached the end of the file.
 			if ((N_BYTES_READ = read(file->fd, BUF, BUFF_SIZE - 1)) < 0)
 			{
 				*r_code = -1;
 				return (result);
 			}
 		}
-		// if it's '\n' then you
-		else if (BUF[i + BUF_IND] == '\n')
-		{
-			grow_string(file, &i, &result, &result_len);
-			break;
-		}
-		if (N_BYTES_READ == 0)
-			return (result);
+		NORM;
 	}
-	// printf("%i\n", i);
-	// printf("%i\n", N_BYTES_READ);
-	// printf("%i\n", BUF_IND);
-	// printf("%i\n", i);
-	// printf("%i\n", i);
-	// printf("%i\n", i);
-	// printf("%i\n", i);
-	// printf("%i\n", i);
-	// printf("%i\n", i);
-	// printf("%i\n", i);
 	return (result);
 }
 
@@ -112,14 +91,11 @@ int		get_next_line(const int fd, char **line)
 	r_code = -1;
 	if (!(file = new_file(fd)) || line == NULL)
 		return (r_code);
-	// if (*line == file->last_stored && *line != NULL) // WTF WON'T YOU WORK!?!?!?!?
-	// 	free(*line); // double check this
 	if (!(result = (char*)ft_memalloc(1)))
 		return (r_code);
 	if (!(result = store_line(file, result, 0, &r_code)))
 		return (-1);
 	file->last_stored = result;
 	*line = result;
-	// return (r_code);
 	return (r_code);
 }
